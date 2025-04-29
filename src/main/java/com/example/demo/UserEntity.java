17743237bec95ab02494e5395d012b6d3fc5f22b
NicatHsynlyv <nicat200507@gmail.com>
2025-04-29 21:21:13 +0400

    first commit

---
+++ src/main/java/com/example/demo/UserEntity.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "userdemo")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+
+}
